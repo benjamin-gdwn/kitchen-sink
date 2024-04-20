b665ddd5a012bd06a58f5618d8793ff305909e54
Ben Goodwin <bnjmn.gdwn@Bens-MacBook-Pro.local>
2024-04-20 09:25:43 +0100

    Initial app bar build

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
+import { HeaderBarDemo } from './LandingPage/HeaderBarDemo';
 
 function App() {
   return (
     <div className="App">
+      <HeaderBarDemo/>
       <header className="App-header">
         <h1>Hello World</h1>
       </header>
---
+++ src/LandingPage/HeaderBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AppBar } from '@mui/material';
+
+export const HeaderBar = () => {
+  return (
+    <AppBar >
+
+    </AppBar>
+  )
+};
